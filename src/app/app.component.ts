45a4ce25a2041a49efd5ebda92e83d7383f9ae1e
Rangana <Rangana@DESKTOP-GC6F9E5>
2024-08-26 03:56:25 +0530

    login ui integration with web api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup,FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'seatapp.web';
+  http=inject(HttpClient);
+
+
+  LoginForm=new FormGroup({
+Email:new FormControl<string|null>(''),
+Password:new FormControl<string|null>('')
+
+  })
+  onLoginSubmited(){
+    const addloginrequest={
+      Email:this.LoginForm.value.Email,
+      Password:this.LoginForm.value.Password
+    }
+    this.http.post('http://localhost:5121/User/Login',addloginrequest).subscribe({
+      next:(value)=>{
+        console.log(value);
+      }
+    })
+  }
+
+
 }
