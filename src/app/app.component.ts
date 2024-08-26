5d4d45b7769782c55f0db98ca3ee104f4c3c2a56
pavani-pathirana <pavani997work@gmail.com>
2024-08-26 10:49:06 +0530

    2nd commit for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+/*import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup,FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -34,4 +34,43 @@ Password:new FormControl<string|null>('')
   }
 
 
+}
+*/
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'seatapp.web';
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  LoginForm = new FormGroup({
+    Email: new FormControl<string | null>(''),
+    Password: new FormControl<string | null>('')
+  });
+
+  onLoginSubmited() {
+    const addloginrequest = {
+      Email: this.LoginForm.value.Email,
+      Password: this.LoginForm.value.Password
+    };
+    this.http.post('http://localhost:5121/User/Login', addloginrequest).subscribe({
+      next: (value) => {
+        console.log(value);
+      }
+    });
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
