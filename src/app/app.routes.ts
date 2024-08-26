acd8dc15aa51c49b0e0b87ba6d4d2c1a7c2c7e24
pavani-pathirana <pavani997work@gmail.com>
2024-08-26 22:25:40 +0530

    Home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { SeatResComponent } from './seat-res/seat-res.component';
+import { HomeComponent } from './home/home.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },  
   { path: 'login', component: LoginComponent },          
   { path: 'register', component: RegisterComponent },   
-  { path: 'seat-res', component:SeatResComponent },    
+  { path: 'seat-res', component:SeatResComponent },  
+  { path: 'home', component:HomeComponent},
 ];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+//import { RouterOutlet} from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+//import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+//import { Router, RouterModule } from '@angular/router';
+//import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  
+
+}
